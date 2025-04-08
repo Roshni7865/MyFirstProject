38d33ea99b14edd61290ad94303e6764e63d7a07
Roshni7865 <rosh7863@gmail.com>
2025-04-08 20:34:34 +0530

    Initial Maven project push

---
+++ src/main/java/Ro/MyfirstProject/App.java
@@ -0,0 +1,13 @@
+package Ro.MyfirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
